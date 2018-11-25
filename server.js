14c328c4b36bec48cefdbb66e63b8204916629ae
Vladimir Klimov <vladimirklimoff@yandex.ru>
2018-11-25 16:03:42 +0500

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-regular-38552',
     user : ***REMOVED***,
     password : ***REMOVED***,
     database : ***REMOVED***
