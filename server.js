2839a64c9370d0e212f28c15362c6e498312e3d7
Vladimir Klimov <vladimirklimoff@yandex.ru>
2019-07-04 18:15:35 +0500

    ident fixes

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-  	connectionString: process.env.DATABASE_URL,
-  	ssl: true,
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
@@ -22,15 +22,15 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors())
 
-app.get('/',(req,res)=>{res.send('it is working!')})
-app.post('/signin', signin.handleSignin(db,bcrypt))
-app.post('/register', (req,res) => { register.handleRegister(req,res,db,bcrypt) })
-app.get('/profile/:id', (req,res) => { profile.handleProfileGet(req,res,db) })
-app.put('/image', (req,res) => { image.handleImage(req,res,db) })
-app.post('/imageurl', (req,res) => { image.handleApiCall(req,res) })
+app.get('/', (req, res) => { res.send('it is working!') })
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(process.env.PORT || 3000,()=>{
-	console.log(`app is running on port ${process.env.PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
 })
 
 
